a25e1046224795cd18266a83267a1e4c6081b372
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-02-22 15:26:33 +0500

    add mongoose and dotenv dependencies; implement database connection in db.js

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
+require("dotenv").config();
+
 const app = require("./src/app")
+const connectDB = require("./src/config/db")
+connectDB();
+
+
 
 app.listen('8080', () => {
     console.log("server is running")
---
+++ src/config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+function connectDB() {
+    mongoose.connect(process.env.MONGO_URI)
+        .then(() => {
+            console.log("connected")
+        })
+        .catch(err => {
+            console.log(err)
+            process.exit(1)
+        })
+}
+
+module.exports = connectDB;

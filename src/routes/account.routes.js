e40ace835f42847abdf64d05e56410f7183a7b27
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-02-25 15:43:51 +0500

    add auth middleware import and enhance route documentation

--- src/routes/account.routes.js
+++ src/routes/account.routes.js
@@ -1,7 +1,15 @@
 const express = require("express");
+const  authMiddleware  = require("../middleware/auth.middleware")
 
 
 const router = express.Router();
+
+
+/**
+ * - POST /api/accounts
+ * - Create a new account 
+ * - Protected route, requires authentication
+ */
 
 
 

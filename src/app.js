485a0d7964d83157a6d9a36c2d6d09996864c7e9
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-02-25 15:22:51 +0500

    organize imports and add account routes file

--- src/app.js
+++ src/app.js
@@ -1,13 +1,24 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
+
+/**
+ * Importing routes
+ */
 const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
 
 const app = express();
 
 
 app.use(express.json())
 app.use(cookieParser())
+
+
+/**
+ * Routes used in the app
+ */
 app.use("/api/auth", authRouter)
+app.use("/api/accounts", accountRouter)
 
 
 

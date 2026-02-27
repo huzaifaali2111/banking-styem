bf63e4079246e82350095baf9ddb4908328feb69
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-02-27 10:35:23 +0500

    Buliding transaction controller

--- src/controllers/transaction.controller.js
+++ src/controllers/transaction.controller.js
@@ -8,12 +8,29 @@ const accountModel = require("../models/account.model")
 
 
 async function createTransaction(req, res) {
+
+    // validate Request 
     const { fromAccount, toAccount, amount, idempotencyKey } = req.body
     if (!fromAccount || toAccount || amount || idempotencyKey) {
         return res.status(400).json({
             message: "fromAccount, toAccount, amount, idempotencyKey is required"
         })
     }
+
+    const fromUserAccount = await accountModel.findOne({
+        _id: fromAccount,
+    })
+    const toUserAccount = await accountModel.findOne({
+        _id: toAccount,
+    })
+    if (!fromUserAccount || toUserAccount) {
+        return res.status(400).json({
+            message: "Invalid Accounts"
+        })
+    }
+
+    // 
+
 
 }
 

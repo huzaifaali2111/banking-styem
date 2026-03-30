54123d4e7f5a8b731773a46feb381560149d6a64
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-03-30 12:29:18 +0500

    initialize Express application with EJS view engine and web routing configuration

--- src/app.js
+++ src/app.js
@@ -1,34 +1,39 @@
 const express = require("express");
 const cookieParser = require("cookie-parser")
+const path = require('path');
+
+// Improting routes
+const authRouter = require("./routes/auth.routes")
+const accountRouter = require("./routes/account.routes")
+const transactionRoutes = require("./routes/transaction.routes")
+const balanceRouter = require("./routes/balance.routes")
+const webRouter = require("./routes/web.routes")
+
 
 
 
 const app = express();
 
+// View Engine
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+// Middleware
 app.use(express.json())
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
 
 
 
-/**
- * Importing routes
- */
-const authRouter = require("./routes/auth.routes")
-const accountRouter = require("./routes/account.routes")
-const transactionRoutes = require("./routes/transaction.routes")
-const balanceRouter = require("./routes/balance.routes")
 
 
 
-
-/**
- * Routes used in the app
- */
+// using routes
 app.use("/api/auth", authRouter)
 app.use("/api/accounts", accountRouter)
 app.use("/api/transactions", transactionRoutes)
 app.use("/api/check",balanceRouter)
+app.use("/",webRouter)
 
 
 

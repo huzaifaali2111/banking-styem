210e713a29918fc1c56f7eb8ed0aa66bac206780
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-04-01 11:01:07 +0500

    implement guest middleware for authentication and add guest view

--- src/middleware/frontendmiddleware/guest.middleware.js
+++ src/middleware/frontendmiddleware/guest.middleware.js
@@ -1,6 +1,23 @@
-// const userModel = require(".../models/user.model")
-async function guest(params) {
-    
+const userModel = require("../../models/user.model")
+const accountModel = require("../../models/account.model")
+const jwt = require("jsonwebtoken")
+
+async function guest(req, res, next) {
+    const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
+    if (!token) {
+        return res.redirect("/auth");
+    }
+
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const user = await userModel.findById(decoded.userId);
+    req.user = user;
+    const isAccount = await accountModel.findById(decoded.userId)
+    if (!isAccount) {
+        return res.redirect("/guest");
+    }
+    return res.redirect("/profile");
 }
 
-module.exports = guest;
+module.exports = {
+    guest
+}
--- src/routes/web.routes.js
+++ src/routes/web.routes.js
@@ -2,14 +2,17 @@ const express = require("express");
 const router = express.Router();
 const middleware = require("../middleware/frontendmiddleware/guest.middleware")
 
-
-router.get("/auth", (req, res) => {
+router.get("/auth",  middleware.guest, (req, res) => {
     res.render("auth");
 })
 
 router.get("/profile", (req, res) => {
     const user = req.cookies.token
     res.render("profile", {user});
+})
+
+router.get("/guest", (req, res)=>{
+    res.render("guest");
 })
 
 module.exports = router

9d7578ed0f7bb75db13564c780fb11620277193f
huzaifaali2111 <mianhuzaifaali62@gmail.com>
2026-04-01 12:59:59 +0500

    refactor validation middleware: improve code formatting and readability; add open account view for guests

--- src/middleware/frontendmiddleware/validation.middleware.js
+++ src/middleware/frontendmiddleware/validation.middleware.js
@@ -7,12 +7,12 @@ async function isloggedIn(req, res, next) {
         const token = req.cookies.token || req.headers.authorization?.split(" ")[1];
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const user = await userModel.findById(decoded.userId);
-        if(user){
+        if (user) {
             return res.redirect("/profile");
         }
-        
+
     } catch (error) {
-        
+
     }
 }
 
@@ -33,11 +33,11 @@ async function requireAuth(req, res, next) {
 }
 
 async function hasAccount(req, res, next) {
-      try {
+    try {
         const token = req.cookies.token;
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const account = await accountModel.findById(decoded.userId);
-        if(!account){
+        if (!account) {
             return res.redirect("/guest");
         }
         return next();
@@ -45,14 +45,15 @@ async function hasAccount(req, res, next) {
     } catch (e) {
         res.send("User have invalid token")
     }
-    
+
 }
+
 async function hasNoAccount(req, res, next) {
-      try {
+    try {
         const token = req.cookies.token;
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
         const account = await accountModel.findById(decoded.userId);
-        if(account){
+        if (account) {
             return res.redirect("/profile");
         }
         return next();
@@ -60,7 +61,7 @@ async function hasNoAccount(req, res, next) {
     } catch (e) {
         res.send("User have invalid token")
     }
-    
+
 }
 
 
